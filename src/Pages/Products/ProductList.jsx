559e6979c10b24b77c700204b1d04f196735e964
Sheetal Bobade <siyapawarsb@gmail.com>
2024-06-12 23:41:11 +0530

    new

--- src/Pages/Products/ProductList.jsx
+++ src/Pages/Products/ProductList.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
